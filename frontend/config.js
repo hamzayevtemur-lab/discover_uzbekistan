7578fce725ca4c7259431c51d8899e468c3a81b5
Temurbek Hamzayev <hamzayevtemur@gmail.com>
2026-05-03 17:50:46 +0800

    api fixed

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 const API = (location.hostname === 'localhost' || location.hostname === '127.0.0.1')
     ? 'http://localhost:8000' : '';
 
-const API_BASE = API; // admin panel uses API_BASE
+const API_BASE     = API;      // admin panel uses API_BASE
+const API_BASE_URL = API;      // index.html, location.html etc use API_BASE_URL
