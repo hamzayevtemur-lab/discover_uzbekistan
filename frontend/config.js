f95ecbb7e85b73f19119b47eae24773206d0fee6
Temurbek Hamzayev <hamzayevtemur@gmail.com>
2026-09-20 15:46:35 +0800

    Make API base URL dynamic in config.js to match current server origin and port

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,6 @@
-const API = (location.hostname === 'localhost' || location.hostname === '127.0.0.1')
-    ? 'http://localhost:8000' : '';
+const API = (location.protocol.startsWith('http')) 
+    ? location.origin 
+    : 'http://localhost:8000';
 
 const API_BASE     = API;      // admin panel
 const API_BASE_URL = API;      // index.html, location.html etc
